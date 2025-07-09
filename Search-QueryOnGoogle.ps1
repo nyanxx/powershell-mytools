3dd7d76dca3647d686c29c0d10393bbb1b0684b2
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-07-09 09:44:49 +0530

    chore(comment): clean up commnets

--- Search-QueryOnGoogle.ps1
+++ Search-QueryOnGoogle.ps1
@@ -1,17 +1,4 @@
-#param([System.String[]]$Args)		# Writing this param() to get $args was the cause for not retrieving the first argument!
-
+# Search on Google
 Add-Type -AssemblyName System.Web
-
-#foreach ($word in $args) {
-#	$sentence = $sentence + " " + $word
-#}
-
-$query = [System.Web.HttpUtility]::UrlEncode($args -join " ")
-
-#$query = [System.Web.HttpUtility]::UrlEncode($sentence.Trim())
-
-#if ($args) { $query = $args -join " " }
-#$query = $query.Replace(" ", "+")
-
+$Query = [System.Web.HttpUtility]::UrlEncode($args -join " ")
 Start-Process "https://www.google.com/search?q=$query&oq=$query&sourceid=chrome&ie=UTF-8"
-
